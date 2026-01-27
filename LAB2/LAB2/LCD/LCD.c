7268a3126f13d872140eb13a1277b411cdd18d37
diegogs04 <gar23787@uvg.edu.gt>
2026-01-27 12:56:11 -0600

    Archivo .h
    Creacion de funciones y variables necesarias para el funcionamiento de LCD

--- LAB2/LAB2/LCD/LCD.c
+++ LAB2/LAB2/LCD/LCD.c
@@ -4,5 +4,7 @@
  * Created: 1/27/2026 11:58:03 AM
  *  Author: diego
  */ 
+#define F_CPU 16000000UL
 #include <avr/io.h>
+#include <util/delay.h>
 #include "LCD.h"
--- LAB2/LAB2/LCD/LCD.h
+++ LAB2/LAB2/LCD/LCD.h
@@ -9,6 +9,13 @@
 #ifndef LCD_H_
 #define LCD_H_
 
+void setup_LCD_8(void);
+void command_LCD(com);
+void char_LCD(car);
+void string_LCD(char *cad);
+void clear(void);
+void cursor_LCD(uint8_t fil, uint8_t col);
+void read_LCD(void);
 
 
 
